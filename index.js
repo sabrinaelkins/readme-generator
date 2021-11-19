4129ccef656f4d6eb92e53198f9c736d01acad96
sabrinaelkins <sabrina.elkins12@gmail.com>
2021-11-19 15:22:35 -0600

    start questionaire

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
+const fs = reqiure('fs');
+const inquirer = require('inquirer');
 
+const readmeGenerator = reqiure('./generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Input your GitHub user',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Input your GitHub username');
+                    return false;
+                }
+            }
+        }
+    ]
+    )
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
