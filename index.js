6ad6a0d0212ecad8350a66b7fc8d6234ea568b82
sabrinaelkins <sabrina.elkins12@gmail.com>
2021-11-19 16:00:11 -0600

    completed questionaire

--- index.js
+++ index.js
@@ -2,33 +2,124 @@
 const fs = reqiure('fs');
 const inquirer = require('inquirer');
 
-const readmeGenerator = reqiure('./generateMarkdown.js');
+// 
+// const readmeGenerator = reqiure('./generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'email',
-            message: 'Insert your email here',
+            name: 'title',
+            message: 'What is the title of your project?',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log('Please input your email here');
+                    console.log('Please input the title of your project here.');
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Insert description of your project here',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input your project description.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Developer',
+            message: 'Insert developers name',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input developer name here.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Installation instructions',
+            message: 'Insert istallment instructions here',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input your installment instructions here. ');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Testing',
+            message: 'Insert testing requirements',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input your testing requirements here. ');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Insert usage conditions here',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input your usage conditions.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'GitHub',
+            message: 'Insert GitHub name here',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input your GitHub name. ');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'Insert email name here',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please input your email. ');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a License',
+            choices: ['MIT','ISC','APACHE2.0', 'GID', 'none'],
         }
     ]
     )
 }
-{
-    type: 'list',
-    name: 'license',
-    message: '',
-    choices: [''],
-},
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
